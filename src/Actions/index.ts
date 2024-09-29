7af1cbb5c805bedec29f917ab34df1f04abd680b
VRealSoft <78587110+vreal-soft@users.noreply.github.com>
2024-09-29 16:44:04 +0300

    update

--- src/Actions/index.ts
+++ src/Actions/index.ts
@@ -1,13 +1,15 @@
 import axios from 'axios'
 import { IDataResponse } from '../types'
 
+const axiosInstance = axios.create();
+
 export const getData = async (data: {
     petitionId: number
     pageNumber: number
 }): Promise<IDataResponse | undefined> => {
     const { petitionId, pageNumber } = data
     try {
-        const response = await axios.get(`/petition/${petitionId}/votes/${pageNumber}/json`)
+        const response = await axiosInstance.get(`https://petition.president.gov.ua/petition/${petitionId}/votes/${pageNumber}/json`)
         if (response) {
             return response.data
         }
