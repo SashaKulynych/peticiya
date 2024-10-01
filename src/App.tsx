b005b86dfd4d349333b5d516fac009f23207d369
VRealSoft <78587110+vreal-soft@users.noreply.github.com>
2024-10-01 21:43:24 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
 import { Chart } from 'react-charts';
@@ -8,7 +8,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import * as Actions from './Actions';
 import ResizableBox from './ResizableBox';
 import { IData } from './types';
-import { Typography } from '@mui/material';
+import { Alert, Stack, Typography } from '@mui/material';
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -17,8 +17,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import classNames from 'classnames';
+
+enum Themes {
+  light = 'light',
+  dark = 'dark'
+}
 
 function App() {
+  const [theme, setTheme] = useState<Themes>(
+    (localStorage.getItem('theme') as Themes) || Themes.dark
+  );
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<IData>([]);
   useEffect(() => {
@@ -32,92 +41,141 @@ function App() {
     };
     init();
   }, []);
+
+  const onChangeTheme = useCallback(() => {
+    setTheme((value) => {
+      let newTheme = Themes.light;
+      if (value === Themes.light) {
+        newTheme = Themes.dark;
+      }
+      localStorage.setItem('theme', newTheme as string);
+      return newTheme;
+    });
+  }, []);
   const count = data
     .map((v) => v.data.length)
     .reduce((partialSum, a) => partialSum + a, 0);
   const percentage = (count * 100) / 25000;
   return (
-    <div className="App">
-      {loading && (
-        <div className="loader-container">
-          <span className="loader"></span>
-        </div>
-      )}
-      <div className="header">
-        <Typography variant="h3" component="h3" fontWeight={700} align="center">
-          Статистика
-        </Typography>
-        <Typography variant="h5" component="h5" align="center">
-          Присвоєння почесного звання Героя України (посмертно) солдату 93
-          окремої механізованої бригади "Холодний Яр" Яцуну Сергію Олеговичу,
-          який загинув, мужньо захищаючи Україну та її народ
-        </Typography>
+    <>
+      <div className="switcher">
+        <label id="switch" className="switch">
+          <input
+            type="checkbox"
+            checked={theme === Themes.dark}
+            onChange={onChangeTheme}
+            id="slider"
+          />
+          <span className="slider round"></span>
+        </label>
       </div>
 
-      {data.length > 0 && (
-        <>
+      <div
+        className={classNames('App', {
+          dark: theme === Themes.dark
+        })}
+      >
+        {loading && (
+          <div className="loader-container">
+            <span className="loader"></span>
+          </div>
+        )}
+        <div className="header">
           <Typography
-            variant="h5"
+            variant="h3"
+            component="h3"
             fontWeight={700}
-            component="h5"
             align="center"
           >
-            {`${count} / 25000`}
+            Статистика
           </Typography>
-          <div className="circle-bar-container">
-            <div className="circle-bar">
-              <CircularProgressbar value={percentage} text={`${percentage}%`} />
-            </div>
-          </div>
+          <Typography variant="h5" component="h5" align="center">
+            Присвоєння почесного звання Героя України (посмертно) солдату 93
+            окремої механізованої бригади "Холодний Яр" Яцуну Сергію Олеговичу,
+            який загинув, мужньо захищаючи Україну та її народ
+          </Typography>
+        </div>
 
-          <ResizableBox>
-            <Chart
-              options={{
-                data: [
-                  {
-                    label: 'Line',
-                    data
-                  }
-                ],
-                primaryAxis: {
-                  getValue: (v: any) => v.date
-                },
-                secondaryAxes: [
-                  {
-                    getValue: (v: any) => v.data.length
-                  }
-                ]
-              }}
-            />
-          </ResizableBox>
-        </>
-      )}
-      <div style={{ margin: 20 }}>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Дата</TableCell>
-                <TableCell align="right">Кількість</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {[...data].reverse().map((row) => (
-                <TableRow
-                  key={row.date}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.date}
-                  </TableCell>
-                  <TableCell align="right">{row.data.length}</TableCell>
+        {data.length > 0 && (
+          <>
+            <Typography
+              variant="h5"
+              fontWeight={700}
+              component="h5"
+              align="center"
+            >
+              {`${count} / 25000`}
+            </Typography>
+            <div className="circle-bar-container">
+              <div className="circle-bar">
+                <CircularProgressbar
+                  value={percentage}
+                  text={`${percentage}%`}
+                />
+              </div>
+            </div>
+
+            <ResizableBox>
+              <Chart
+                options={{
+                  data: [
+                    {
+                      label: 'Line',
+                      data
+                    }
+                  ],
+                  primaryAxis: {
+                    getValue: (v: any) => v.date
+                  },
+                  secondaryAxes: [
+                    {
+                      getValue: (v: any) => v.data.length
+                    }
+                  ]
+                }}
+              />
+            </ResizableBox>
+          </>
+        )}
+        <div style={{ margin: 20 }}>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Дата</TableCell>
+                  <TableCell align="right">Кількість</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              {data.length === 0 && (
+                <TableBody>
+                  <TableRow>
+                    <TableCell colSpan={5}>
+                      <Stack sx={{ width: '100%' }} spacing={2}>
+                        <Alert severity="info">No Data !</Alert>
+                      </Stack>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              )}
+
+              <TableBody>
+                {[...data].reverse().map((row) => (
+                  <TableRow
+                    key={row.date}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.date}
+                    </TableCell>
+                    <TableCell align="right">{row.data.length}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
